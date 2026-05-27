78af458d6d67474663aebfa46e5297a5aeee6bd8
Volodya Datsyshyn <imvovo4kaa@gmail.com>
2026-05-27 22:28:37 +0300

    feat: added managers.py and models.py

--- app/managers.py
+++ app/managers.py
@@ -12,7 +12,8 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self.connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?)",
+            f"INSERT INTO {self.table_name} "
+            "(first_name, last_name) VALUES (?, ?)",
             (first_name, last_name,)
         )
         self.connection.commit()
